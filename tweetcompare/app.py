4889b92da97d8b0986b87a3d4dd4cf72456bd67b
will-cotton4 <will.cotton4@gmail.com>
2019-06-18 15:16:50 -0500

    added files for day 2 assignment

--- tweetcompare/app.py
+++ tweetcompare/app.py
@@ -1,15 +1,23 @@
 """Main application and routing logic for TweetCompare."""
-from flask import Flask
-from .models import DB
+from os import getenv
+from pickle import dump, loads
+from flask import Flask, render_template, request
+from .models import DB, User
+from .twitter import add_or_update_user
+from decouple import config
+
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['ENV'] = config('ENV')
     DB.init_app(app)
-    
+
     @app.route("/")
     def root():
-        return "Welcome to TweetCompare!"
+        users = User.query.all()
+        return render_template('base.html', title='Home', users=users)
 
     return app
