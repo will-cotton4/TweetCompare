1c21684f2810655456a7073590b0c12078e902fb
will-cotton4 <will.cotton4@gmail.com>
2019-06-20 12:01:17 -0500

    updated sqlalchemy database url

--- tweetcompare/app.py
+++ tweetcompare/app.py
@@ -1,4 +1,5 @@
 """Main application and routing logic for TweetCompare."""
+import os
 from os import getenv
 from pickle import dump, loads
 from flask import Flask, render_template, request
@@ -10,7 +11,8 @@ from decouple import config
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['ENV'] = config('ENV')
     DB.init_app(app)
